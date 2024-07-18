2cce06812c8ff1786c085d92fd4abd2ccb7567cd
jaimito512 <156543092+jaimito512@users.noreply.github.com>
2024-07-17 23:25:58 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -1,23 +1,8 @@
 package main
 
-import "fmt"
-
-type digit []int
-
 func main() {
 
-	b := []int{}
-	for i := 0; i < 11; i += 1 {
-		b = append(b, i)
-	}
-	fmt.Println(b)
-
-	for j := 1; j < 11; j += 1 {
-		if b[j]%2 == 0 {
-			fmt.Println(j, "is even")
-		} else {
-			fmt.Println(j, "is odd")
-		}
-
-	}
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
 }
