2b1834c568bb1b36c28257a809f67d4087f053a4
jaimito512 <156543092+jaimito512@users.noreply.github.com>
2024-07-17 23:25:24 -0500

    Assignment: Even or Odd
    Assignment from-
    Go: The Complete Developer's Guide (Golang)
    
    -Creating Slices
    -Iterating through slice and calling out the different values

--- main.go
+++ main.go
@@ -2,9 +2,22 @@ package main
 
 import "fmt"
 
+type digit []int
+
 func main() {
 
-	cards := newDeck()
-	cards.toString()
-	fmt.Println(cards.toString())
+	b := []int{}
+	for i := 0; i < 11; i += 1 {
+		b = append(b, i)
+	}
+	fmt.Println(b)
+
+	for j := 1; j < 11; j += 1 {
+		if b[j]%2 == 0 {
+			fmt.Println(j, "is even")
+		} else {
+			fmt.Println(j, "is odd")
+		}
+
+	}
 }
